78c7814ada053010f76de99d3e6efe6a078b35b6
Adam Fetters <adamfetterscoder@gmail.com>
2018-03-09 12:37:21 -0500

    Couldn't get stretch problem to work correctly

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -10,6 +11,7 @@ int main(int argc, char **argv)
   // Parse command line
   DIR *pDir;
   struct dirent *ent;
+  struct stat buf;
 
   if (argc < 2)
   {
@@ -31,11 +33,12 @@ int main(int argc, char **argv)
   }
   while ((ent = readdir(pDir)) != NULL)
   {
+    // stat(ent->d_name, &buf);
+    // printf("file size is %lld\n", (unsigned int)buf.st_size);
     printf("%s\n", ent->d_name);
   }
   // Close directory
   closedir(pDir);
 
-  
   return 0;
 }
