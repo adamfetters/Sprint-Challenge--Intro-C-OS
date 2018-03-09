5c6c4a5976b5c37a758a46c8911a9f7d6b3706ac
Adam Fetters <adamfetterscoder@gmail.com>
2018-03-09 12:18:23 -0500

    Refactored

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -14,39 +14,28 @@ int main(int argc, char **argv)
   if (argc < 2)
   {
     //TODO open current directory
-    printf("Usage: testprog <dirname>\n");
-    pDir = opendir(".");
-    if (pDir == NULL)
-    {
 
-      printf("Cannot open directory %s\n", argv[1]);
-      exit(0);
-    }
-    while ((ent = readdir(pDir)) != NULL)
-    {
-      printf("%s\n", ent->d_name);
-    }
-    closedir(pDir);
+    pDir = opendir(".");
   }
   else
   {
     pDir = opendir(argv[1]);
-    if (pDir == NULL)
-    {
-
-      printf("Cannot open directory %s\n", argv[1]);
-      exit(0);
-    }
-    while ((ent = readdir(pDir)) != NULL)
-    {
-      printf("%s\n", ent->d_name);
-    }
-    closedir(pDir);
   }
 
   // Repeatly read and print entries
+  if (pDir == NULL)
+  {
 
+    printf("Cannot open directory %s\n", argv[1]);
+    exit(0);
+  }
+  while ((ent = readdir(pDir)) != NULL)
+  {
+    printf("%s\n", ent->d_name);
+  }
   // Close directory
+  closedir(pDir);
 
+  
   return 0;
 }
